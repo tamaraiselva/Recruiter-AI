0d62913b9af9e4e49fab1b2915ce82a3f1a14e51
tamaraiselva <tamaraiselvan98@gmail.com>
2024-06-28 10:12:36 +0530

    cors enable the main.py file

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from routes.job import router as jd_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*",
+    "http://localhost",
+    "http://localhost:8000" 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(jd_router, prefix="/api/v1/jd", tags=["Job Descriptions"])
 
